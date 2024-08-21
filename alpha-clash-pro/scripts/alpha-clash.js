b7a485d07a323eafc94edfdfbea373b5e6935c48
KaziMithfa <kazimithfa99@gmail.com>
2024-08-21 14:26:55 +0200

    alphabet clash project

--- alpha-clash-pro/scripts/alpha-clash.js
+++ alpha-clash-pro/scripts/alpha-clash.js
@@ -16,6 +16,96 @@
 
 // }
 
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+    console.log('player pressed',playerPressed);
+
+    //stop the game if player press ESC
+    if(playerPressed === "Escaped"){
+        gameOver();
+    }
+
+
+    //get the expected to press
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed,expectedAlphabet);
+
+    // check matched or not
+    if(playerPressed === expectedAlphabet){
+        console.log("you got a point")
+
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementById('current-score',updatedScore);
+
+
+
+
+
+
+
+        // ---------
+        //update score
+        //1.get the current score
+        // const currentScroeElement = document.getElementById('current-score');
+        // const currentScoreText = currentScroeElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // console.log(currentScore);
+
+       
+
+
+
+        //2. increase the score by 1
+
+        // const newScore = currentScore + 1;
+
+        //3. show the updated score
+
+        // currentScroeElement.innerText = newScore;
+
+
+
+        //start a new round
+        removeColorById(expectedAlphabet);
+        continueGame();
+    }
+
+    else{
+
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementById('current-life',updatedLife);
+
+        if(updatedLife == 0){
+           gameOver();
+        }
+        
+        //-------
+        //step1: get the current life number
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        // //step2: reduce the current Life by 1
+
+        // const newLife = currentLife - 1;
+
+        // //update
+        // currentLifeElement.innerText = newLife;
+
+
+    }
+
+    
+
+}
+
+document.addEventListener('keyup',handleKeyboardButtonPress)
+
 function continueGame(){
     //step 1: Generate a random alphabet
 
@@ -31,7 +121,34 @@ function continueGame(){
 }
 
 function play(){
+    //hide everything and show only the playground
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('playground');
+
+    //reset score and life
+    setTextElementById('current-life',5);
+    setTextElementById('current-score',0)
+
     continueGame();
+}
+
+function gameOver(){
+    hideElementById('playground');
+    showElementById('final-score');
+
+    //update final score
+    //1. get the final score
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementById('game-score',lastScore);
+
+    // clear the last selected alphabet highlight
+
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeColorById(currentAlphabet);
+   
+
+
+
+
 }
