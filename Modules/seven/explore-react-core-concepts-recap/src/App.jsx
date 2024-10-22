4cb6ec71d09c2227b4c113da5d04e2ea42f11b20
KaziMithfa <kazimithfa99@gmail.com>
2024-10-22 14:16:50 +0200

    Exploring JS concepts

---
+++ Modules/seven/explore-react-core-concepts-recap/src/App.jsx
@@ -0,0 +1,23 @@
+import Posts from "./Posts";
+
+function App() {
+  return (
+    <>
+      <h3>React Core concepts</h3>
+      <ol>
+        <li>Components</li>
+        <li>JSX</li>
+        <li>Props</li>
+        <li>Event Handler</li>
+        <li>State</li>
+        <li>Load Data</li>
+      </ol>
+
+      <hr />
+
+      <Posts></Posts>
+    </>
+  );
+}
+
+export default App;
