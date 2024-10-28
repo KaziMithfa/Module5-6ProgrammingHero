6bd68d2371ebbc96d213dbbd44b579225a06349b
KaziMithfa <kazimithfa99@gmail.com>
2024-10-28 14:12:49 +0100

    fifth commit

--- Modules/seven/memorable-water-bottle/src/components/Bottles/Bottles.jsx
+++ Modules/seven/memorable-water-bottle/src/components/Bottles/Bottles.jsx
@@ -2,22 +2,60 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import "./Bottles.css";
+import { addToLS, getStoredCart } from "../../utilitites/localStorage";
+import Cart from "../Cart/Cart";
 
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
+  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     fetch("bottles.json")
       .then((res) => res.json())
       .then((data) => setBottles(data));
   }, []);
 
+  // load card from local storage
+
+  useEffect(() => {
+    console.log("called the useEffect", bottles.length);
+
+    if (bottles.length) {
+      const storedCart = getStoredCart();
+      //console.log(storedCart,bottles);
+      const savedCart = [];
+
+      for (const id of storedCart) {
+        console.log(id);
+        const bottle = bottles.find((bottle) => bottle.id === id);
+        if (bottle) {
+          savedCart.push(bottle);
+        }
+      }
+      console.log(savedCart);
+      setCart(savedCart);
+    }
+  }, [bottles]);
+
+  const handleAddToCart = (bottle) => {
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+    addToLS(bottle.id);
+  };
+
   return (
     <div>
-      <h2>Bottles here: {bottles.length}</h2>
+      <h2>Bottles Available: {bottles.length}</h2>
+      {/* <h4>Cart: {cart.length}</h4> */}
+      <Cart cart={cart}></Cart>
 
       <div className="bottle-container">
         {bottles.map((bottle) => (
-          <Bottle key={bottle.id} bottle={bottle}></Bottle>
+          <Bottle
+            key={bottle.id}
+            bottle={bottle}
+            handleAddToCart={handleAddToCart}
+          ></Bottle>
         ))}
       </div>
     </div>
