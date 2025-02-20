54784e20d0e58fe10a54ef2c3905d61ef4048a54
KaziMithfa <kazimithfa99@gmail.com>
2025-02-20 16:24:37 +0100

    started MongoDB again

--- Modules/ten/simple-crud-client/src/components/Users.jsx
+++ Modules/ten/simple-crud-client/src/components/Users.jsx
@@ -1,8 +1,20 @@
-import React from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const users = useLoaderData();
+  const handleDelete = (_id) => {
+    console.log("delete", _id);
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("deleted successfully");
+        }
+      });
+  };
   return (
     <div>
       <h2>{users.length}</h2>
@@ -10,6 +22,7 @@ const Users = () => {
         {users.map((user) => (
           <p key={user._id}>
             {user.name} : {user.email}
+            <button onClick={() => handleDelete(user._id)}>X</button>
           </p>
         ))}
       </div>
