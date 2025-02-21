5575bd4a8400f1b4552f7c4a08715201b0622b9c
KaziMithfa <kazimithfa99@gmail.com>
2025-02-21 12:59:21 +0100

    MongoDB crud operations learned

--- Modules/ten/simple-crud-client/src/components/Users.jsx
+++ Modules/ten/simple-crud-client/src/components/Users.jsx
@@ -1,7 +1,9 @@
-import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Users = () => {
-  const users = useLoaderData();
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
   const handleDelete = (_id) => {
     console.log("delete", _id);
     fetch(`http://localhost:5000/users/${_id}`, {
@@ -12,6 +14,8 @@ const Users = () => {
         console.log(data);
         if (data.deletedCount > 0) {
           alert("deleted successfully");
+          const remaining = users.filter((user) => user._id !== _id);
+          setUsers(remaining);
         }
       });
   };
@@ -21,7 +25,10 @@ const Users = () => {
       <div>
         {users.map((user) => (
           <p key={user._id}>
-            {user.name} : {user.email}
+            {user.name} : {user.email} {user._id}
+            <Link to={`/update/${user._id}`}>
+              <button>Update</button>
+            </Link>
             <button onClick={() => handleDelete(user._id)}>X</button>
           </p>
         ))}
