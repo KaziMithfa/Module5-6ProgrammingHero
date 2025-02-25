931f32220bff91fe320b038bcfdd0049134d5eb6
KaziMithfa <kazimithfa99@gmail.com>
2025-02-25 23:49:49 +0100

    learning CRUD operations

--- Modules/ten/coffee-store-client/src/App.jsx
+++ Modules/ten/coffee-store-client/src/App.jsx
@@ -1,10 +1,32 @@
+import { useLoaderData } from "react-router-dom";
 import "./App.css";
+import CoffeeCard from "./components/CoffeeCard";
+import { useState } from "react";
 
 function App() {
+  const loadedcoffes = useLoaderData();
+  const [coffes, setCoffes] = useState(loadedcoffes);
+
   return (
-    <>
-      <h1 className="text-red-600">Vite + React</h1>
-    </>
+    <div className="m-20">
+      <h1
+        className="text-center text-6xl my-20
+       text-center"
+      >
+        {coffes.length}
+      </h1>
+
+      <div className="grid md:grid-cols-2 gap-4">
+        {coffes.map((coffee) => (
+          <CoffeeCard
+            key={coffee._id}
+            coffee={coffee}
+            coffes={coffes}
+            setCoffes={setCoffes}
+          ></CoffeeCard>
+        ))}
+      </div>
+    </div>
   );
 }
 
---
+++ Modules/ten/coffee-store-client/src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEqcmmncLMjR_8LRNCbvxLzypMcwiM4og",
+  authDomain: "coffee-store-b9f07.firebaseapp.com",
+  projectId: "coffee-store-b9f07",
+  storageBucket: "coffee-store-b9f07.firebasestorage.app",
+  messagingSenderId: "260964205996",
+  appId: "1:260964205996:web:e26f7125bd4f45e53a6609",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
