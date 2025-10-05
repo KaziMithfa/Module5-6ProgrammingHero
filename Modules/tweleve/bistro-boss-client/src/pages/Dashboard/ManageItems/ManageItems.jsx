88ea0226756ea6d314f4468905ca3709019e1cef
KaziMithfa <kazimithfa99@gmail.com>
2025-10-05 22:54:53 +0200

    some menu items deleted

--- Modules/tweleve/bistro-boss-client/src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ Modules/tweleve/bistro-boss-client/src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -2,11 +2,36 @@ import React from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
 import { FaEdit, FaTrash } from "react-icons/fa";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ManageItems = () => {
   const [menu] = useMenu();
+  const axiosSecure = useAxiosSecure();
 
-  const handleDeleteItem = (item) => {};
+  const handleDeleteItem = (item) => {
+    Swal.fire({
+      title: "Are you sure to delete this item?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        console.log("Item ID to delete:", item._id);
+        const res = await axiosSecure.delete(`/menu/${item._id}`);
+        console.log(res.data);
+
+        // Swal.fire({
+        //   title: "Deleted!",
+        //   text: "Your file has been deleted.",
+        //   icon: "success",
+        // });
+      }
+    });
+  };
 
   return (
     <div>
